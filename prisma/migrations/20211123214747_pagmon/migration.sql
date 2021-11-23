4c02fabf143c2fbf32bb1286b4ae7cf4d601475e
TGMaelstrom <tgmaelstrom@gmail.com>
2021-11-23 15:56:23 -0600

    Anadida db Monedas

--- prisma/migrations/20211123214747_pagmon/migration.sql
+++ prisma/migrations/20211123214747_pagmon/migration.sql
@@ -20,3 +20,20 @@ CREATE TABLE "pagos" (
 
     CONSTRAINT "pagos_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateTable
+CREATE TABLE "monedas" (
+    "soporte_moneda_id" VARCHAR(255) NOT NULL,
+    "soporte_idioma_id" VARCHAR(255),
+    "soporte_moneda_clave" VARCHAR(255),
+    "soporte_moneda_nombre" VARCHAR(255),
+    "soporte_moneda_decimales" VARCHAR(255),
+    "soporte_moneda_porcentaje_variacion" VARCHAR(255),
+    "soporte_moneda_nombre_singular" VARCHAR(255),
+    "soporte_moneda_nombre_plural" VARCHAR(255),
+    "soporte_moneda_denominacion" VARCHAR(255),
+    "soporte_moneda_simbolo" VARCHAR(255),
+    "soporte_moneda_status" VARCHAR(255),
+
+    CONSTRAINT "monedas_pkey" PRIMARY KEY ("soporte_moneda_id")
+);
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,14 +5,13 @@ import { monedas } from "./seeds/monedasData"
 const prisma= new PrismaClient()
 
 async function main(){
-    /*
     await prisma.pagos.createMany({
         data:pagos,
     })
     await prisma.monedas.createMany({
         data:monedas
     })
-    */
+    
 }
 main().catch(e=>{
     console.log(e)
